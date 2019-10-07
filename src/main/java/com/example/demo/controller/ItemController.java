aeb917bee026d51d9771d586f8e6f01573ed1d68
yukihiro-yokogawa <yukihiro.yokogawa@rakus-partners.co.jp>
2019-10-07 09:35:07 +0900

    session 整理

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.example.demo.domain.Item;
-import com.example.demo.domain.Paging;
 import com.example.demo.form.ItemForm;
 import com.example.demo.form.SearchCategoryForm;
 import com.example.demo.service.ItemService;
@@ -26,17 +25,12 @@ import com.google.common.collect.Iterables;
 
 @Controller
 @RequestMapping("/item")
-@SessionAttributes(value = "searchCategoryForm")
+@SessionAttributes(types = {SearchCategoryForm.class,ItemForm.class,Item.class})
 public class ItemController {
 
 	@Autowired
 	ItemService itemService;
-
-	//商品検索した値を保持する.
-	@ModelAttribute("searchCategoryForm")
-	public SearchCategoryForm searchCategoryForm() {
-		return new SearchCategoryForm();
-	}
+	
 	@ModelAttribute
 	public ItemForm setUpItemForm() {
 		return new ItemForm();
@@ -61,15 +55,17 @@ public class ItemController {
 		List<Item> itemList = itemService.selectItem(1, "select", searchCategoryForm);
 		model.addAttribute("itemList", itemList);
 		// ページの最後の商品のIDを取得
-		session.setAttribute("maxId", Iterables.getLast(itemList).getId());
+		searchCategoryForm.setMaxId(Iterables.getLast(itemList).getId());
+		// ページの最初の商品のIDを取得（null対策）
+		searchCategoryForm.setMinId(itemList.get(0).getId());
 
 		// 商品の全件数を検索
 		Item countItem = itemService.selectItem("count", searchCategoryForm);
 		Integer maxPage = (countItem.getCount() / 30) + 1;
-		session.setAttribute("maxPage", maxPage);
+		searchCategoryForm.setMaxPage(maxPage);
 
 		// トップページなのでページ数1を指定する
-		model.addAttribute("page", 1);
+		searchCategoryForm.setPage(1);
 		
 		model.addAttribute("searchCategoryForm",searchCategoryForm);
 		return "list";
@@ -78,74 +74,56 @@ public class ItemController {
 	/**
 	 * 検索用のview
 	 * 
-	 * @param paging
 	 * @param rs
 	 * @param model
 	 * @param session
 	 * @return
 	 */
 	@RequestMapping("/searchItem")
-	public String searchItem(@Validated Paging paging, BindingResult rs, Model model, HttpSession session,SearchCategoryForm searchCategoryForm) {
-		// 変数を初期化
-		Integer nextId = 1;
-		Integer prevId = 1;
-		Integer maxPage = 1;
+	public String searchItem(@Validated SearchCategoryForm searchCategoryForm, BindingResult rs, Model model) {
+
+		Integer nextId = searchCategoryForm.getMaxId() + 1;
+		Integer prevId = searchCategoryForm.getMinId() - 1;
+		Integer maxPage = searchCategoryForm.getMaxPage();
 		List<Item> itemList = new ArrayList<>();
-		
-		if(session.getAttribute("maxPage") != null){
-			//nullチェック
-			maxPage = Integer.parseInt(String.valueOf(session.getAttribute("maxPage")));
-		}
-
-		if (session.getAttribute("maxId") != null) {
-			// nullチェック ページング next用
-			nextId = Integer.parseInt(String.valueOf(session.getAttribute("maxId"))) + 1;
-		}
-
-		if (session.getAttribute("minId") != null) {
-			// nullチェック ページング prev用
-			prevId = Integer.parseInt(String.valueOf(session.getAttribute("minId"))) - 1;
-		}
-
-		if (paging.getType().equals("next")) {
+	
+		if (searchCategoryForm.getType().equals("next")) {
 			// nextが押されたとき
-			itemList = itemService.selectItem(nextId, paging.getType(), searchCategoryForm);
-		} else if (paging.getType().equals("prev")) {
+			itemList = itemService.selectItem(nextId, searchCategoryForm.getType(), searchCategoryForm);
+		} else if (searchCategoryForm.getType().equals("prev")) {
 			// prevが押されたとき
-			itemList = itemService.selectItem(prevId, paging.getType(), searchCategoryForm);
+			itemList = itemService.selectItem(prevId, searchCategoryForm.getType(), searchCategoryForm);
+			//DESCで呼び出してきたものをASCに並び替える
 			Collections.reverse(itemList);
-		} else if (paging.getType().equals("search")) {
+		} else if (searchCategoryForm.getType().equals("search")) {
 			// ページ検索されたとき
-			if (paging.getPage() == 1) {
+			if (searchCategoryForm.getPage() == 1) {
 				//検索直後必ず1ページ目に遷移させる、フォームに1を入れてもこちらがはしる
 				Item countItem = itemService.selectItem("searchCount", searchCategoryForm);
 				itemList = itemService.selectItem(countItem.getMinId(), "searchItem", searchCategoryForm);
 			} else {
 				//入力欄に数字を入れて検索するとき
-				Integer offset = ((paging.getPage() - 1) * 30);
-				itemList = itemService.selectItem(offset, paging.getType(), searchCategoryForm);
+				Integer offset = ((searchCategoryForm.getPage() - 1) * 30);
+				itemList = itemService.selectItem(offset, searchCategoryForm.getType(), searchCategoryForm);
 			}
 		} else {
 			//検索したい商品の総数とそれ全体の最小のIDを取得（検索条件を絞りSQLを高速化するため）
 			Item countItem = itemService.selectItem("searchCount", searchCategoryForm);
-			itemList = itemService.selectItem(countItem.getMinId(), paging.getType(), searchCategoryForm);
+			itemList = itemService.selectItem(countItem.getMinId(), searchCategoryForm.getType(), searchCategoryForm);
 			maxPage = (countItem.getCount() / 30) + 1;
 			//null対策
-			paging.setPage(1);
+			searchCategoryForm.setPage(1);
 		}
 
 		//表示された商品の中の最大のID（ページング用）
-		session.setAttribute("maxId", Iterables.getLast(itemList).getId());
+		searchCategoryForm.setMaxId(Iterables.getLast(itemList).getId());
 		//表示された商品の中の最小のID（ページング用）
-		session.setAttribute("minId", itemList.get(0).getId());
-		
+		searchCategoryForm.setMinId(itemList.get(0).getId());		
 		model.addAttribute("itemList", itemList);
-		session.setAttribute("maxPage", maxPage);
-		
-		//現在のページ数を送信
-		model.addAttribute("page", paging.getPage());
-		
 		model.addAttribute("searchCategoryForm",searchCategoryForm);
+		
+		searchCategoryForm.setMaxPage(maxPage);
+			
 		return "list";
 	}
 	
@@ -155,10 +133,9 @@ public class ItemController {
 	 * @return
 	 */
 	@RequestMapping("/detail")
-	public String itemDetail(Integer itemId,@ModelAttribute Paging paging, SearchCategoryForm searchCategoryForm, Model model,HttpSession session) {
+	public String itemDetail(SearchCategoryForm searchCategoryForm, ItemForm itemForm, Model model) {
 		
-		System.out.println(paging);
-		Item itemDetail = itemService.itemDetail(itemId, paging.getType());
+		Item itemDetail = itemService.itemDetail(Integer.parseInt(itemForm.getId()), searchCategoryForm.getType());
 		if(itemDetail.getParentName() == null) {
 			itemDetail.setParentName("no-category");
 			itemDetail.setChildName("no-category");
@@ -178,24 +155,14 @@ public class ItemController {
 		detailMap.put("condition", String.valueOf(itemDetail.getCondition()));
 		detailMap.put("description", itemDetail.getDescription());
 		
-		//SessionAttributesを再送信
-		model.addAttribute("searchCategoryForm",searchCategoryForm);
-		model.addAttribute("itemId",itemId);
-		model.addAttribute("page",paging.getPage());
-		model.addAttribute("type",paging.getType());
+		model.addAttribute("itemForm",itemForm);
 		model.addAttribute("detailMap",detailMap);
 		model.addAttribute("itemDetail",itemDetail);
 		return "detail";
 	}
 	
 	@RequestMapping("/edit")
-	public String itemEdit(ItemForm itemForm, Model model, Paging paging,Integer itemId, SearchCategoryForm searchCategoryForm) {
-		System.out.println(paging);
-		model.addAttribute("itemDetail",itemForm);
-		model.addAttribute("itemId",itemId);
-		model.addAttribute("page",paging.getPage());
-		model.addAttribute("type",paging.getType());
-		model.addAttribute("searchCategoryForm",searchCategoryForm);
+	public String itemEdit(Item itemDetail, Model model) {
 		return "edit";
 	}
 
--- src/main/java/com/example/demo/form/SearchCategoryForm.java
+++ src/main/java/com/example/demo/form/SearchCategoryForm.java
@@ -26,4 +26,28 @@ public class SearchCategoryForm {
 	/** ブランド名 */
 	String brand;
 	
+	/**	検索したいページ */
+	Integer page;
+
+	/**	最大ページ */
+	Integer maxPage;
+	
+	/**
+	 * ページの検索タイプ
+	 * viewで
+	 * topページを表示したときはselect
+	 * nextを押した場合next
+	 * prevを押した場合prev
+	 * ページ検索した場合search
+	 * 商品検索をした場合searchItem
+	 * brand名を押して検索したときはsearchBrand
+	 * 商品詳細のページに遷移した際はitemDetail
+	 * 
+	 */
+	String type;
+	
+	Integer maxId;
+	
+	Integer minId;
+	
 }
